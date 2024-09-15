014e63df4a2e0897bdbc9a9a23d8bb113f48c481
masonhaines <levi.haines97@gmail.com>
2024-09-14 21:14:03 -0700

    created test c++ code

---
+++ Source/Githubtest/Githubtest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Githubtest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Githubtest, "Githubtest" );
---
+++ Source/Githubtest/MyTest2.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyTest2.h"
+
+MyTest2::MyTest2()
+{
+}
+
+MyTest2::~MyTest2()
+{
+	int test;
+	test = 1;
+}
