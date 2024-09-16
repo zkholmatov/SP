d54c1bb415a07782b0b92ccc2639a98afcc314aa
masonhaines <levi.haines97@gmail.com>
2024-09-16 14:54:59 -0700

    added enemy  BP and animation(instance) - adjusted template map -

---
+++ Source/Githubtest/MyEnemyAnimInstance.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyEnemyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITHUBTEST_API UMyEnemyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "variables")
+	float currentVelocity{0.0f};
+
+};
