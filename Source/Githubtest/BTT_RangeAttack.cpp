557cfb0e8bdaca4f133b68d93601fa654c8ab8c2
masonhaines <levi.haines97@gmail.com>
2024-09-18 11:22:45 -0700

    Created range attack class, added FX asssets

---
+++ Source/Githubtest/BTT_RangeAttack.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTT_RangeAttack.h"
+#include "AIController.h"
+#include"GameFramework/Character.h"
+
+EBTNodeResult::Type UBTT_RangeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// UE_LOG(LogTemp, Warning, TEXT("Success!"));
+	ACharacter* CharacterRef{OwnerComp.GetAIOwner()->GetPawn<ACharacter>()};
+
+	if (!IsValid(CharacterRef)) { return EBTNodeResult::Failed; };
+	CharacterRef->PlayAnimMontage(AnimMontage);
+	
+	return EBTNodeResult::Succeeded;
+}
